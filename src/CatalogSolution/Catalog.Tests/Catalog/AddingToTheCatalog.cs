8f7cd420781e39372a2a01f892ecd64cb08e3167
Jeffry Gonzalez <jeff@hypertheory.com>
2024-07-08 15:30:54 -0400

    added first systems test

---
+++ src/CatalogSolution/Catalog.Tests/Catalog/AddingToTheCatalog.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace Catalog.Tests.Catalog;
+public class AddingToTheCatalog
+{
+
+    [Fact]
+    public void DoIt()
+    {
+        // a test that lets add things to the catalog.
+        // when I do an HTTP POst to some resource in our API with some entity (body) then I get a response with the following.
+    }
+}
