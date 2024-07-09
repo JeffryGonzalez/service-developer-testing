47edba832db3271b1da09632983f962c1c91b76b
Jeffry Gonzalez <jeff@hypertheory.com>
2024-07-09 10:41:54 -0400

    after feature toggles

--- src/CatalogSolution/Catalog.Api/Program.cs
+++ src/CatalogSolution/Catalog.Api/Program.cs
@@ -1,7 +1,8 @@
 using Catalog.Api.Catalog;
+using Microsoft.FeatureManagement;
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Services.AddFeatureManagement();
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -12,12 +13,16 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI();
 
+}
+
+// Todo: Talk about Feature Flags Tomorrow.
+if (await app.Services.GetRequiredService<IFeatureManager>().IsEnabledAsync("Catalog"))
+{
     app.MapCatalog();
 }
-app.UseSwagger();
-app.UseSwaggerUI();
-// Todo: Talk about Feature Flags Tomorrow.
 app.Run();
 
 public partial class Program { }
